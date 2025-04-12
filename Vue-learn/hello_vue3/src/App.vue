15f73b71b9a8e145ed28b8d4be4c474f051b305d
YDpandas <yangda314314@gmail.com>
2025-04-12 16:58:39 +0800

    123

---
+++ Vue-learn/hello_vue3/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+        <h1>HelloWorld!</h1>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name: 'App'
+    }
+</script>
+
+<style>
+    .hello{
+        background-color: #b8dca7;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
