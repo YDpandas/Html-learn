eea716fcb717725d31b009126cbca7ca04947419
pandaYDs <yangda314314@gmail.com>
2025-08-29 21:05:08 +0800

    Update Person.vue

---
+++ Vue-learn/hello_vue3/src/components/Person.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="person">
+
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name: 'Person'
+    }
+</script>
+
+<style>
+    .person{
+        background-color: #569639;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
